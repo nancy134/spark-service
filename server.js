4c689ce2601353505f83905d630f9b480d4ee237
Bill Piedra <Bill@Bills-iMac-770.local>
2021-10-12 08:05:25 -0400

    first comit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("spark-service");
+});
+
+app.listen(PORT, HOST);
