fd17a84a7321e1dedcabcb736ed4fa5f340d630c
Bill Piedra <Bill@Bills-iMac-770.local>
2022-02-08 09:29:32 -0500

    sync contacts

--- sqs.js
+++ sqs.js
@@ -3,12 +3,19 @@ AWS.config.update({region: 'us-east-1'});
 const syncCCContact = process.env.AWS_SQS_CC_SYNC_CONTACT_TO_SPARK
 
 const { Consumer } = require('sqs-consumer');
-
+const sparkService = require('./spark');
 
 exports.handleSQSMessage = function(message){
     var json = JSON.parse(message.Body);
     var json2 = JSON.parse(json.Message);
-    console.log(json2);
+    var queryStr = "?email="+json2.email;
+    var accessToken = json2.token;
+    sparkService.getContacts(accessToken, queryStr).then(function(contacts){
+        console.log(contacts);
+    }).catch(function(err){
+        console.log(err);
+    });
+
 }
 
 exports.sqsApp = Consumer.create({
